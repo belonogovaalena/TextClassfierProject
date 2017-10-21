c6bc6850ce078f64ce2c41bb890555974bba3006
belonogovaalena <alena.belonogova@yandex.ru>
2017-10-21 23:55:45 +0300

    upd

--- result/src/main/java/ru/caf82/result/machinelearning/preprocessing/SequenceProcessor.java
+++ result/src/main/java/ru/caf82/result/machinelearning/preprocessing/SequenceProcessor.java
@@ -1,7 +1,24 @@
-package ru.caf82.result.machinelearning.preprocessing;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package result.machinelearning.preprocessing;
+
+import java.util.List;
+import java.util.Map;
 
 /**
- * Created by ilysko on 03.08.17.
+ *
+ * @author User
  */
 public interface SequenceProcessor {
+    public List<Map<String, Integer>> fitAndTransform(List<String> listOfTexts);
+
+   public void fit(List<String> listOfTexts);
+
+   public List<String> preprocess(String text);
+
+   public List<Map<String, Integer>> transform(List<String> listOfTexts);
+
 }
