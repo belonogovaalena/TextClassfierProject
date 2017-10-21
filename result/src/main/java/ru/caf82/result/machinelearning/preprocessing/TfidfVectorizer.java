e2ca56f919f28a0e65855811d48bb67954010ea3
belonogovaalena <alena.belonogova@yandex.ru>
2017-10-21 23:56:29 +0300

    upd

--- result/src/main/java/ru/caf82/result/machinelearning/preprocessing/TfidfVectorizer.java
+++ result/src/main/java/ru/caf82/result/machinelearning/preprocessing/TfidfVectorizer.java
@@ -4,4 +4,24 @@ package ru.caf82.result.machinelearning.preprocessing;
  * Created by ilysko on 03.08.17.
  */
 public class TfidfVectorizer implements SequenceProcessor, Transformer {
+
+    @Override
+    public List<Map<String, Integer>> fitAndTransform(List<String> listOfTexts) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void fit(List<String> listOfTexts) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<String> preprocess(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Map<String, Integer>> transform(List<String> listOfTexts) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
