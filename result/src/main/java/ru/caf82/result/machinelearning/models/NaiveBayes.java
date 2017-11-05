d6c92774d883722957de32def426df386573d9ba
belonogovaalena <alena.belonogova@yandex.ru>
2017-11-05 21:59:31 +0400

    updated NB

--- result/src/main/java/ru/caf82/result/machinelearning/models/NaiveBayes.java
+++ result/src/main/java/ru/caf82/result/machinelearning/models/NaiveBayes.java
@@ -5,30 +5,26 @@
  */
 package ru.caf82.result.machinelearning.models;
 
-/**
- *
- * @author Алена
- */
 import java.io.IOException;
 import ru.caf82.result.exceptions.InconveninentShapeException;
-import java.io.Serializable;
 
 /**
  *
  * @author Алена
  */
-public class NaiveBayes implements MlModel, Serializable {
+public class NaiveBayes implements MlModel {
    private float alpha;
    private double[][] weights;
    private boolean fitted;
    private boolean parallel;
    public NaiveBayes(float alpha, boolean parallel) {
        //Миша сказал забить
-    }
+   }
+   
    public MlModel train(double[][] X, int[] y) throws InconveninentShapeException {
       //считаем у=0 и y=1
       if (X.length != y.length) 
-          throw new InconveninentShapeException();
+          throw new InconveninentShapeException("Неверные входные параметры.");
       double yZer = 0, yOne = 0;
       for (int i=0; i<y.length; i++) {
           if (y[i]==0)
@@ -45,35 +41,48 @@ public class NaiveBayes implements MlModel, Serializable {
       for (int j=0; j<X[0].length; j++) {
           double xZer=0, xOne = 0;
           for (int k=0; k<X.length; k++) {
-              if ((X[k][j]==1)&& (y[k]==1))
+              if ((X[k][j]==1) && (y[k]==1))
                  xOne++;
-              if ((X[k][j]==1)&& (y[k])==0)
+              if ((X[k][j]==1) && (y[k])==0)
                   xZer++;
           }
           xOneProb[j] = xOne/yOne;
           xZerProb[j] = xZer/yZer;
       }
+      
+      weights = new double[4][];
+      weights[0] = new double[X[0].length];
+      weights[1] = new double[X[0].length];
+      weights[2] = new double[1];
+      weights[3] = new double[1];
+      
       for (int k=0; k<X[0].length; k++) {
-      weights[0][k] = xOneProb[k];
-      weights[1][k] = xZerProb[k];
+         weights[0][k] = xOneProb[k];
+         weights[1][k] = xZerProb[k];
+      }
       weights[2][0] = yOneProb;
       weights[3][0] = yZerProb;
-              }
-       fitted = true;
+      fitted = true;
       return this;
     }
-   public int[] predict(double[] X) throws  InconveninentShapeException {
+   
+   public int[] predict(double[] X) throws ModelNotFittedException, InconveninentShapeException {
        double[] wasPredicted = predictProba(X);
        int[] myPredict = new int[X.length];
        for (int i =0; i <X.length; i++) {
            if (wasPredicted[i]<0.5)
-            myPredict[i]=0;
+               myPredict[i]=0;
            else
                myPredict[i]=1;
        }
        return myPredict;
     }
-   public double[] predictProba(double[] X) throws  InconveninentShapeException {
+   
+   public double[] predictProba(double[] X) throws ModelNotFittedException, InconveninentShapeException {
+       if (!isFitted())
+           throw new ModelNotFittedException("Модель не обучена");
+       if (X.length!=weights[0].length)
+           throw new InconveninentShapeException("Неверные входные параметры");
        double[] myPredict = new double[X.length];
        for (int i=0; i<X.length; i++) {
            double znam = (weights[0][i]*weights[2][0])+(weights[1][i]*weights[3][0]);
@@ -95,10 +104,8 @@ public class NaiveBayes implements MlModel, Serializable {
    
    public double[][] getWeights() {
        return weights; 
-       
    } 
    public boolean isFitted() {
-       return fitted;
-       
+       return fitted; 
    }
 }
